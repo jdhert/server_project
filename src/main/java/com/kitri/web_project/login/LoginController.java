f7d3e5ebb7d7ab2387780e1e7c24e9d5ff54ebb1
jdhert <https://github.com/jdhert/my-servlet-board.git>
2024-02-26 11:15:56 +0900

    -m

---
+++ src/main/java/com/kitri/web_project/login/LoginController.java
@@ -0,0 +1,10 @@
+package com.kitri.web_project.login;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+}
