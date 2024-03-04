e11f701677903e3b4e4c928154b1b3c291f4386d
jdhert <https://github.com/jdhert/my-servlet-board.git>
2024-03-04 17:14:52 +0900

    서버 서칭바 보완중

--- src/main/java/com/kitri/web_project/controller/FreeboardController.java
+++ src/main/java/com/kitri/web_project/controller/FreeboardController.java
@@ -1,12 +1,10 @@
 package com.kitri.web_project.controller;
 
 import com.kitri.web_project.dto.QnaInfo;
+import com.kitri.web_project.dto.board.RequestBoard;
 import com.kitri.web_project.mybatis.mappers.BoardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,10 +23,35 @@ public class FreeboardController {
         if(page == 1)
             offset = 0;
         else offset = (page - 1) * maxPage + (page - 2) * (maxPage / 2);
-        limit = 10;
+        limit = 8;
         List<QnaInfo> qnaInfos = boardMapper.getFreeBoards(offset, limit);
         if(qnaInfos.isEmpty())
             return null;
         else return qnaInfos;
+    }
+
+    @PostMapping
+    public void uploadBoard(@RequestBody RequestBoard board){
+        boardMapper.uploadQna(board);
+    }
+
+    @GetMapping("/search/{page}")
+    public List<QnaInfo> search(@RequestParam String search,@RequestParam String type, @PathVariable int page){
+        int maxPage=8;
+        int offset;
+        int limit;
+        if(page == 1)
+            offset = 0;
+        else offset = (page - 1) * maxPage + (page - 2) * (maxPage / 2);
+        limit = 8;
+        switch (type){
+            case "writer":
+                return boardMapper.getWritFreeBoards(search+"%", offset, limit);
+            case "content":
+                return boardMapper.getContFreeBoards(search+"%", offset, limit);
+            case "tag":
+                return boardMapper.getTagFreeBoards(search+"%", offset, limit);
+        }
+        return null;
     }
 }
