290b915a02567aa1c2936c8a002de6025c4eaa82
DESKTOP-V0IM9A7\kitri <na9173@suwon.ac.kr>
2024-03-08 16:57:43 +0900

    - pet Carousel 데이터 불러오기 - pet Diary 등록하기

--- src/main/java/com/kitri/web_project/controller/UserInfoController.java
+++ src/main/java/com/kitri/web_project/controller/UserInfoController.java
@@ -2,13 +2,12 @@ package com.kitri.web_project.controller;
 
 import com.kitri.web_project.dto.DiaryInfo;
 import com.kitri.web_project.dto.PetInfo;
+import com.kitri.web_project.dto.diary.RequestDiary;
 import com.kitri.web_project.mybatis.mappers.UserMapper;
 import com.kitri.web_project.dto.UserInfo;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,23 +20,33 @@ public class UserInfoController {
 
 
     @GetMapping("/{id}")
-    public UserInfo getInfo(@PathVariable String id){
+    public UserInfo getInfo(@PathVariable String id) {
         long id1 = Long.parseLong(id);
         return userMapper.findById(id1);
     }
 
     @GetMapping("/pet/{id}")
-    public List<PetInfo> getPet(@PathVariable String id){
+    public List<PetInfo> getPet(@PathVariable String id) {
         long id1 = Long.parseLong(id);
         return userMapper.getPets(id1);
     }
 
     @GetMapping("/diary/{id}")
-    public List<DiaryInfo> getDiary(@PathVariable String id){
+    public List<DiaryInfo> getDiary(@PathVariable String id) {
         long id1 = Long.parseLong(id);
         return userMapper.getDiary(id1);
     }
 
+    @PostMapping
+    public void adddiary(@RequestBody RequestDiary diaryInfo) {
+        userMapper.save(diaryInfo);
+    }
+    //다이어리 등록 매핑
 
-
+    @GetMapping("/select/{id}")
+    public List<RequestDiary>requestDiaries(@PathVariable long id){
+//        long id1 = Long.parseLong(id);
+        return userMapper.petDiary(id);
+    }
+    //다이어리 미리보기 매핑
 }
