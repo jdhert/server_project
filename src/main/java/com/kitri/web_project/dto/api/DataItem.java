20ca7cdc4a9080984e234515d3ee310f31d82a14
jdhert <https://github.com/jdhert/my-servlet-board.git>
2024-03-19 17:55:23 +0900

    공공 api 로컬 DB로 업로드 완료

--- src/main/java/com/kitri/web_project/dto/api/DataItem.java
+++ src/main/java/com/kitri/web_project/dto/api/DataItem.java
@@ -1,15 +1,106 @@
 package com.kitri.web_project.dto.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 public class DataItem {
-    private String 시설명;
+    @JsonProperty("건물 번호")
+    private String buildingNumber;
 
-    private String 위도;
-    private String 경도;
-    private String 지번주소;
+    @JsonProperty("경도")
+    private String longitude;
+
+    @JsonProperty("기본 정보_장소설명")
+    private String basicInfoPlaceDescription;
+
+    @JsonProperty("도로명 이름")
+    private String roadName;
+
+    @JsonProperty("도로명주소")
+    private String roadAddress;
+
+    @JsonProperty("리 명칭")
+    private String villageName;
+
+    @JsonProperty("반려동물 동반 가능정보")
+    private String petCompanionInfo;
+
+    @JsonProperty("반려동물 전용 정보")
+    private String petExclusiveInfo;
+
+    @JsonProperty("반려동물 제한사항")
+    private String petRestriction;
+
+    @JsonProperty("번지")
+    private String addressNumber;
+
+    @JsonProperty("법정읍면동명칭")
+    private String jurisdictionTownName;
+
+    @JsonProperty("시군구 명칭")
+    private String districtName;
+
+    @JsonProperty("시도 명칭")
+    private String cityName;
+
+    @JsonProperty("시설명")
+    private String facilityName;
+
+    @JsonProperty("애견 동반 추가 요금")
+    private String additionalPetFee;
+
+    @JsonProperty("우편번호")
+    private Integer postalCode;
+
+    @JsonProperty("운영시간")
+    private String operatingHours;
+
+    @JsonProperty("위도")
+    private String latitude;
+
+    @JsonProperty("입장 가능 동물 크기")
+    private String petSizeAdmission;
+
+    @JsonProperty("입장(이용료)가격 정보")
+    private String admissionFeeInfo;
+
+    @JsonProperty("장소(실내) 여부")
+    private String indoorPlaceInfo;
+
+    @JsonProperty("장소(실외)여부")
+    private String outdoorPlaceInfo;
+
+    @JsonProperty("전화번호")
+    private String phoneNumber;
+
+    @JsonProperty("주차 가능여부")
+    private String parkingAvailability;
+
+    @JsonProperty("지번주소")
+    private String lotAddress;
+
+    @JsonProperty("최종작성일")
+    private String lastModifiedDate;
+
+    @JsonProperty("카테고리1")
+    private String category1;
+
+    @JsonProperty("카테고리2")
+    private String category2;
+
+    @JsonProperty("카테고리3")
+    private String category3;
+
+    @JsonProperty("홈페이지")
+    private String website;
+
+    @JsonProperty("휴무일")
+    private String dayOff;
+
     private double distance;
+
+    private int maxPage=22955;
 }
---
+++ src/main/java/com/kitri/web_project/dto/api/TestDto.java
@@ -0,0 +1,9 @@
+package com.kitri.web_project.dto.api;
+
+import lombok.Data;
+
+@Data
+public class TestDto {
+    String category;
+    int page;
+}
