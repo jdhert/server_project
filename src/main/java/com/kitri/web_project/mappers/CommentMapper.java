8b7cd00390bb685c08e944f0aec47f73f0b652db
kimmisung <kimmisung22@naver.com>
2024-04-03 19:46:38 +0900

    detailboard _댓글 좋아요 이슈

--- src/main/java/com/kitri/web_project/mappers/CommentMapper.java
+++ src/main/java/com/kitri/web_project/mappers/CommentMapper.java
@@ -2,8 +2,10 @@ package com.kitri.web_project.mappers;
 
 import com.kitri.web_project.dto.comment.CommentDto;
 import com.kitri.web_project.dto.comment.RequestComment;
+import com.kitri.web_project.dto.comment.RequestCommentLike;
 import com.kitri.web_project.dto.comment.RequestReplyComment;
 import org.apache.ibatis.annotations.Mapper;
+import org.eclipse.tags.shaded.org.apache.regexp.RE;
 
 import java.util.List;
 
@@ -28,18 +30,24 @@ public interface CommentMapper {
 
     int childCount(long id);
 
-    boolean checkCommentLikeExists(long userId, long boardId, long commentId);
-    boolean insertCommentLike(long userId, long boardId, long commentId);
-    boolean deleteCommentLike(long userId, long boardId, long commentId);
+//    boolean checkCommentLikeExists(long userId, long boardId, long commentId);
+//    boolean insertCommentLike(long userId, long boardId, long commentId);
+//    boolean deleteCommentLike(long userId, long boardId, long commentId);
+//
+//    boolean getCommentLikeStatus(Long commentId);
 
-    boolean getCommentLikeStatus(Long commentId);
+    boolean checkCommentLikeExists(RequestCommentLike requestCommentLike);
+    boolean insertCommentLike(RequestCommentLike requestCommentLike);
+    boolean deleteCommentLike(RequestCommentLike requestCommentLike);
+
+    boolean getCommentLikeStatus(RequestCommentLike requestCommentLike);
 
 
-    boolean checkReplyLikeExists(long userId, long boardId, long commentId);
-    boolean insertReplyLike(long userId, long boardId, long commentId);
-    boolean deleteReplyLike(long userId, long boardId, long commentId);
+//    boolean checkReplyLikeExists(long userId, long boardId, long commentId);
+//    boolean insertReplyLike(long userId, long boardId, long commentId);
+//    boolean deleteReplyLike(long userId, long boardId, long commentId);
 
-    boolean getReplyLikeStatus(Long replylId);
+//    boolean getReplyLikeStatus(Long replylId);
 
     int getTotalCommentCount(long boardId);
 
